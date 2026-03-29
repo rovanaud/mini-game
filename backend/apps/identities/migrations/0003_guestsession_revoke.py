7b38e985d2d06fbc78646621e00f855abdb3f6cf
Ivan Tchomba <nguekorovanaud@gmail.com>
2026-03-29 03:39:55 +0200

    fix: resolve looping migration

--- backend/apps/identities/migrations/0003_guestsession_revoke.py
+++ backend/apps/identities/migrations/0003_guestsession_revoke.py
@@ -1,10 +1,9 @@
 
-# migration 0002_guestsession_revoke.py
 from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-    dependencies = [("identities", "0002_guestsession_revoke")]
+    dependencies = [("identities", "0002_useridentity_remove_guestsessionid")]
     operations = [
         migrations.RemoveField(
             model_name="useridentity",
